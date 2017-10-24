8ce6e337b393d163d323a0ecef821c7b5eccc82f
Courtney <codavis@redventures.com>
2017-10-24 11:59:42 -0400

    Initial Commit

---
+++ CSharpToy/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+            //var friends = new List<string>();
+            //var finished = false;
+            //Console.WriteLine("Please enter names one at a time. When you're done, hit enter with a blank line.");
+            //// take input from the reader
+
+            //do
+            //{
+            //    var input = Console.ReadLine();
+            //    if (input != "")
+            //    {
+            //        //add input to list
+
+            //        friends.Add(input);
+            //    }
+            //    else
+            //    {
+            //        finished = true;
+            //    }
+
+
+            //} while (!finished);
+
+            //if (friends.Count < 2)
+            //{
+            //    Console.WriteLine(friends[0] + " likes your post.");
+            //}
+            //else if (friends.Count < 3)
+            //{
+            //    Console.WriteLine(friends[0] + " and " + friends[1] + " like your post.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(friends[0] + ", " + friends[1] + " and " + (friends.Count - 2) + " others like your post.");
+            //}
+
+            //var name = "Courtney";
+            //var chars = new List<char>();
+            //foreach (var n in name) {
+            //    chars.Add(n);
+            //}
+            //chars.Reverse();
+            //foreach (var i in chars)
+            //{
+            //    Console.Write(i);
+            //}
+            //Console.WriteLine();
+
+
+        }
+    }
+}
